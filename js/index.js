ee5b99b1e4e091be58c972e10a5025c46f983a60
陈庆取 <chenqingqu@fingard.com>
2023-05-10 20:55:29 +0800

    首页index.js文件新建

---
+++ js/index.js
@@ -0,0 +1,79 @@
+$('#slider').nivoSlider({
+  effect: 'random',
+  slices: 15,
+  boxCols: 8,
+  boxRows: 4,
+  animSpeed: 500,
+  pauseTime: 6000,
+  startSlide: 0,
+  directionNav: true,
+  controlNav: true,
+  pauseOnHover: true,
+  manualAdvance: false,
+  prevText: 'Prev',
+  nextText: 'Next',
+  randomStart: true,
+});
+jQuery(function ($) {
+  $(window).load(function () {
+
+  });
+});
+
+jQuery(".slideBox").slide({ mainCell: ".bd ul", effect: "leftLoop", autoPlay: true });
+
+
+jQuery(".picScroll-left").slide({ titCell: ".hd ul", mainCell: ".bd ul", autoPage: true, effect: "leftLoop", autoPlay: true, vis: 2 });
+
+
+$(function () {
+  $('#SubmitAdd').click(function () {
+    var username = $('#username').val();
+    var phone = $('#phone').val();
+    var text = $('#text').val();
+    // $.post("/tijiao",null,function () {
+    //
+    // })
+    $.ajax({
+      url: "/tijiao",
+      type: 'POST',
+      data: { username: username, phone: phone, text: text },
+      dataType: 'json',
+      success: function (data) {
+        if (data.code == 1) {
+          alert(data.msg);
+          location.reload();
+        } else {
+          alert(data.msg);
+        }
+      }
+    });
+    return false;
+  });
+});
+
+$(function () {
+  $('#SubmitAdds').click(function () {
+    var username = $('#username').val();
+    var phone = $('#phone').val();
+    var text = $('#text').val();
+    // $.post("/tijiao",null,function () {
+    //
+    // })
+    $.ajax({
+      url: "/tijiao",
+      type: 'POST',
+      data: { username: username, phone: phone, text: text },
+      dataType: 'json',
+      success: function (data) {
+        if (data.code == 1) {
+          alert(data.msg);
+          location.reload();
+        } else {
+          alert(data.msg);
+        }
+      }
+    });
+    return false;
+  });
+});
